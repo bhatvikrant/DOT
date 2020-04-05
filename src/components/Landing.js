0f5ad73fc4a82397c34240dedd3cb28633152a01
Vikrant Bhat <vik.bhat223@gmail.com>
2020-04-06 01:26:38 +0530

    homepage icons added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import '../css/Landing.css';
+
+// Icons
+import digitalPhamphlets from '../assets/homepageIcons/digitalPhamphlets.png';
+import saveExhibitors from '../assets/homepageIcons/saveExhibitors.png';
+import utilizeContacts from '../assets/homepageIcons/utilizeContacts.png';
+
 import { Row, Col } from 'react-bootstrap';
 const Landing = () => {
 	return (
-		<div className='container-fluid p-0 bg-danger' style={{ height: '320px' }}>
+		<div
+			className='container-fluid p-0 bg-danger landing__bg'
+			style={{ height: '320px' }}
+		>
 			<Row className='d-inline'>
 				<Col>
 					<h3 className='text-center'>
@@ -10,18 +20,68 @@ const Landing = () => {
 					</h3>
 				</Col>
 			</Row>
-			<div className='container'>
+			<div className='container text-center justify-content'>
 				<Row>
-					<Col>
-						<h6>Get Digital Pamphlets, say no to heavy bags</h6>
+					<Col align='center' md={4} xs={4}>
+						<h6 className='min__height__60'>Get Digital Pamphlets</h6>
+
+						<div
+							style={{
+								background: 'rgba(0, 0, 0, 0.8)',
+								height: '150px',
+								width: '150px',
+							}}
+							className='rounded-circle custom__icon__bg'
+						>
+							<img
+								src={digitalPhamphlets}
+								alt='Get Digital Pamphlets, say no to heavy bags'
+								height='140'
+								className='custom__icon'
+							/>
+						</div>
 					</Col>
 
-					<Col>
-						<h6>Save your Favourite Exhibitors</h6>
+					<Col align='center' md={4} xs={4}>
+						<h6 className='min__height__60'>Save your Favourite Exhibitors</h6>
+
+						<div
+							style={{
+								background: 'rgba(0, 0, 0, 0.8)',
+								height: '150px',
+								width: '150px',
+							}}
+							className='rounded-circle custom__icon__bg'
+						>
+							<img
+								src={saveExhibitors}
+								alt='Save your Favourite Exhibitors'
+								height='140'
+								className='custom__icon'
+							/>
+						</div>
 					</Col>
 
-					<Col>
-						<h6>Utilize Contacts at Your Comfort</h6>
+					<Col align='center' md={4} xs={4}>
+						<h6 className='min__height__60'>
+							Utilize Contacts at Your Comfort
+						</h6>
+
+						<div
+							style={{
+								background: 'rgba(0, 0, 0, 0.8)',
+								height: '150px',
+								width: '150px',
+							}}
+							className='rounded-circle custom__icon__bg'
+						>
+							<img
+								src={utilizeContacts}
+								alt='Utilize Contacts at Your Comfort'
+								height='140'
+								className='custom__icon'
+							/>
+						</div>
 					</Col>
 				</Row>
 			</div>
